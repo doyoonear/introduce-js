3a1eaa5556aebc2b242a5948fb401594886a8136
Doyoonear <doyoonleee@gmail.com>
2020-07-15 15:48:04 +0900

    Second Commit

--- todo.js
+++ todo.js
@@ -31,7 +31,7 @@ function paintToDo(text){
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length + 1;
-    delBtn.innerText = "🗑";
+    delBtn.innerText = "✔️";
     delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
