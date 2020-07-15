fc58a988f22dbb189706a68392d68ef8eeb424bd
Doyoonear <doyoonleee@gmail.com>
2020-07-15 23:38:23 +0900

    ADD Modal

---
+++ modal.js
@@ -0,0 +1,16 @@
+const openButton = document.getElementById("open");
+const modal = document.querySelector(".modal");
+const overlay = modal.querySelector(".modal__overlay");
+const closeButton = modal.querySelector("button");
+
+const openModal = () => {
+    modal.classList.remove("hidden");
+}
+
+const closeModal = () => {
+    modal.classList.add("hidden");
+}
+
+overlay.addEventListener("click", closeModal);
+closeButton.addEventListener("click", closeModal);
+openButton.addEventListener("click", openModal);
