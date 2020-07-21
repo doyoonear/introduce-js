20f104c83aeeef65bd6d82dc8559d3e766daa4fc
Doyoonear <doyoonleee@gmail.com>
2020-07-21 15:35:45 +0900

    modal.js 복구

--- modal.js
+++ modal.js
@@ -2,7 +2,7 @@ const openButton = document.getElementById("open");
 const modal = document.querySelector(".modal");
 const overlay = modal.querySelector(".modal__overlay");
 const closeButton = modal.querySelector("button");
-
+// modal 복구
 const openModal = () => {
     modal.classList.remove("hidden");
 }
